dae5a763a84e08a8b1fe978f552c66bb0ac5228b
Pablo Roca <lpablord28@gmail.com>
2022-01-27 21:37:10 -0600

    Entrega de pedido

--- Practicas/Practica_1/microservicio-repartidor/controller/repartidor.controller.js
+++ Practicas/Practica_1/microservicio-repartidor/controller/repartidor.controller.js
@@ -86,10 +86,83 @@ exports.informarPedido  = async (req, res) => {
 }
 
 exports.entregado = async (req, res) => {
-    let body = req.body;
-    res.send({
-        'status':200,
-        'msj': 'Usuario Registrado con exito',
-        'data': []
+    //let body = req.body;
+    //const token = body.token;
+    const token = req.headers['access-token'];
+    
+    //---------------------
+    var axios = require('axios');
+    var data = JSON.stringify({});
+    var config = {
+        method: 'post',
+        url: 'http://localhost:9000/validarR',
+        headers: { 
+            'access-token': token, 
+            'Content-Type': 'application/json'
+        },
+        data : data
+    };
+
+    axios(config)
+    .then(function (response) {
+    var valido = response.data;
+    if(valido != undefined && valido.valido){
+        if(valido.type === 1){
+            console.log("<EntregandoPedido>Marcando como entregado el pedido");
+            /*const payload = {
+                id:valido.user_id,
+                user:valido.user,
+                type:valido.type,
+                pedido:body.pedido,
+                estadoRT:body.estadoRT,
+                estadoRP:body.estadoRP
+            };
+            
+            const token2 = jwt.sign(payload, llave, {
+                expiresIn: 1440
+            });*/
+            if(valido.estadoRP){
+                console.log("<VerificandoPedido>Pedido ya entregado");
+                res.send({
+                    'status':200,
+                    'msj': "Pedido Ya entregado",
+                    'data': token
+                });
+            }else{
+                response.estadoRP = true;
+                console.log("<VerificandoPedido>Pedido siendo entregado");
+                res.send({
+                    'status':200,
+                    'msj': "Pedido aun no ha sido aceptado por restaurante",
+                    'data': token
+                });
+            }
+            
+        }else{
+            console.log("<VerificacionFallido>Entrega invalida");
+            res.send({
+                'status':401,
+                'msj': "Verificacion Invalida",
+                'data': []
+            });
+        }
+        
+    }else{
+        console.log("<PedidoFallido>No tiene permisos para utiilzar esta api");
+        res.send({
+            'status':401,
+            'msj': "Este usuario no existe o no cuenta con permisos para utilizar el api",
+            'data': []
+        });
+    }
+    //return JSON.stringify(response.data);
+    })
+    .catch(function (error) {
+    console.log(error);
+        res.send({
+            'status':400,
+            'msj': "Este usuario no existe",
+            'data': []
+        });
     });
 }
