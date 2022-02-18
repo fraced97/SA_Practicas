c4ed35e3b56faf1e52141322ca8a83320807b901
Pablo Roca <lpablord28@gmail.com>
2022-02-17 18:11:52 -0600

    Login y testing

--- Practicas/Practica-3/src/app/app.component.spec.ts
+++ Practicas/Practica-3/src/app/app.component.spec.ts
@@ -19,17 +19,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have as title 'Practica-3'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('Practica-3');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('Practica-3 app is running!');
-  });
 });
