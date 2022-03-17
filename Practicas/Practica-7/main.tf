fefc2993b13472eba2e542d1ed969d21fc9b93e1
Pablo Roca <lpablord28@gmail.com>
2022-03-17 17:56:50 -0600

    Terraform

---
+++ Practicas/Practica-7/main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 2.15.0"
+    }
+  }
+}
+
+provider "docker" {}
+
+resource "docker_image" "practica7" {
+  name         = "fraced97/pareja6"
+  keep_locally = false
+}
+
+resource "docker_container" "pareja6" {
+  image = docker_image.practica7.name
+  name  = "pareja6"
+  ports {
+    internal = 80
+    external = 80
+  }
+}
